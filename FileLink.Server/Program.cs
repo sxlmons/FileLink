e48079e8b9597387c9238212e53b2956d94edec8
Joshua Salmons <joshuasalmons@icloud.com>
2025-03-13 00:13:03 -0400

    {feat}: Add User class, Logger Interface, Commands
    - User class: id, username, password(salt hash), email, timestamped
    - Commands: Ideas under construction, leading with command codes for requests/responses
    - Logger interface: laid out the framework for the logging functionality
    - Logger class: writes to a file under the logging directory and as server transitions states the logger will write logs of the server state to file

--- FileLink.Server/Program.cs
+++ FileLink.Server/Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿namespace FileLink.Server
+{
+    // Serves as the main entry point of the program, will initialize 
+    // the server engine and begin listening for connections.
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Start logging server states
+            
+            // Load server configuration
+            
+            // Initialize and start the server
+            
+            // shutdown
+        }
+    }
+}
---
+++ FileLink.Server/Services/Logging/LogService.cs
@@ -0,0 +1,21 @@
+namespace FileLink.Server.Services.Logging
+{
+    // Centralized logging service that can log to multiple loggers
+    // Also provides packet logging functionality
+    public class LogService 
+    {
+        // implement ILogger
+        
+        // initialize logservice class
+        
+        // Add Logger
+        
+        // Log message with specified level
+        
+        // log message with specified level and exception
+        
+        // log packet being sent or received
+        
+        // 
+    }
+}
