cdae1acd3a0525ebb3d50b8a09753f4be3bfe3f1
Joshua Salmons <joshuasalmons@icloud.com>
2025-03-16 06:24:03 -0400

    Removed .NET MAUI Project, Updated gitignore
    - gitignore updated with official template from microsoft
    - removed any instance of the .NET MAUI project

--- FileLink.Client/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FileLink.Client;
-
-public partial class AppShell : Shell
-{
-    public AppShell()
-    {
-        InitializeComponent();
-    }
-}
--- FileLink.Client/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace FileLink.Client;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
