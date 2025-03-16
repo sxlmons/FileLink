f195f0a53336119034e7bd9914d2fe69104ebb5e
Joshua Salmons <joshuasalmons@icloud.com>
2025-03-16 06:34:37 -0400

    Add .NET MAUI Project for Client
    - Fixed issue where project would be pulled with build files

---
+++ FileLink.Client/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+
+namespace FileLink.Client;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new AppShell());
+    }
+}
---
+++ FileLink.Client/AppShell.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace FileLink.Client;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
