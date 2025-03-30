80864b58eb58c96e296c60b762ba1be4971e1bfd
Joshua Salmons <joshuasalmons@icloud.com>
2025-03-29 23:42:26 -0400

    wip(DirectoryService): Implemented skeleton code for directory services class
    Note: this is a work in progress the skeleton structure is subject to change

--- FileLink.Server/Disk/DirectoryManagement/DirectoryService.cs
+++ FileLink.Server/Disk/DirectoryManagement/DirectoryService.cs
@@ -11,5 +11,64 @@ namespace FileLink.Server.Disk.DirectoryManagement
         private readonly IFileRepository _fileRepository;
         private readonly PhysicalStorageService _storageService;
         private readonly LogService _logService;
+        
+        // Initializes a new instance of the DirectoryService class
+        public DirectoryService(IDirectoryRepository directoryRepository, IFileRepository fileRepository, PhysicalStorageService storageService, LogService logService)
+        {
+            _directoryRepository = directoryRepository ?? throw new ArgumentNullException(nameof(directoryRepository));
+            _fileRepository = fileRepository ?? throw new ArgumentNullException(nameof(fileRepository));
+            _storageService = storageService ?? throw new ArgumentNullException(nameof(storageService));
+            _logService = logService ?? throw new ArgumentNullException(nameof(logService));
+        }
+
+        public async Task<DirectoryMetadata> CreateDirectory(string userId, string directoryName, string parentDirectoryId = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<DirectoryMetadata>> GetAllDirectories(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<DirectoryMetadata>> GetDirectoriesInDirectory(string userId, string parentDirectoryId = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<(IEnumerable<FileMetadata> Files, IEnumerable<DirectoryMetadata> Directories)> GetDirectoryContents(string userId, string directoryId = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> RenameDirectory(string directoryId, string newName, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> DeleteDirectory(string directoryId, string userId, bool recursive = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> MoveFilesToDirectory(IEnumerable<string> fileIds, string targetDirectoryId, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> MoveFileToDirectory(string fileId, string targetDirectoryId, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<DirectoryMetadata> GetDirectoryById(string directoryId, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string SanitizeDirectoryName(string directoryName)
+        {
+            return directoryName;
+        }
     }
 }
